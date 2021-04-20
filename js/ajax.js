ec1f337ae18ead3145ad4b48f23de7e366234e48
Afonso Barbosa <afonso33barbosa@gmail.com>
2021-04-20 09:54:56 -0400

    alterado formato da campanha

--- js/ajax.js
+++ js/ajax.js
@@ -3,14 +3,8 @@ let campanha = 4;
 function formulario_1(){
   let nome_cliente = document.querySelector('#nome_1').value;
   let telefone_cliente = document.querySelector('#telefone_1').value;
-  let unidade_cliente = document.querySelector('#cidade_1').value;
-  var interesses_1 = document.querySelectorAll('.interesses_1:checked');
-  var values = [];
-
-  for(let i = 0; i < interesses_1.length; i ++){
-
-    values.push(interesses_1[i].value);
-  }
+  let unidade_cliente = 1;
+  var interesse = 'Cliente veio da campanha do complexo primavera, tem interesse em fazer um orçamento sobre os planos.';
 
   swal({
     title: "Agora falta pouco!",
@@ -31,13 +25,12 @@ function formulario_1(){
       telefone_cliente: telefone_cliente,
       unidade_cliente: unidade_cliente,
       id_campanha: campanha,
-      descricao_observacao: values
+      descricao_observacao: interesse
     },
   
     success: function() {
       $('#nome_1').val("");
       $('#telefone_1').val("");
-      $('#cidade_1').val("");
 
       swal({
         title: "Recebemos o seu contato!",
